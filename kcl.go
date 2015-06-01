5ec1e43e4cdff2a0f5eeab881f2cbd444aa4b7e3
matt price <matt.price@mandiant.com>
2015-06-01 13:26:02 -0400

    documentation updates, testing travis

--- kcl.go
+++ kcl.go
@@ -10,6 +10,6 @@ The quickest way to get started using this package is to set the following
 environmental variables: Access Key ID: AWS_ACCESS_KEY_ID or AWS_ACCESS_KEY,
 Secret Access Key: AWS_SECRET_ACCESS_KEY or AWS_SECRET_KEY, and region: AWS_REGION.
 
-Please log and issues or feature requests here: https://github.com/suicidejack/kinesis_client_library/issues.
+Please report/log issues or feature requests here: https://github.com/suicidejack/kinesis_client_library/issues.
 */
 package kcl
